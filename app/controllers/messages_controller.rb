b7bbf37428df310f3f25142ab2e007ec0a6318cb
sj <grinpeace101@gmail.com>
2017-05-09 10:57:53 +0000

    DRY-共通化

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy ]
+  
   def index
     @messages = Message.all
   end
 
   def show
-    @message = Message.find(params[:id])
+    #@message = Message.find(params[:id])
+    #set_message
   end
 
   def new
@@ -15,7 +18,8 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     if @message.save
       flash[:success] = 'Message が正常に投稿されました'
-      redirect_to messages_url
+      #redirect_to messages_url
+      redirect_to @message
     else
       flash.now[:danger] = 'Message が投稿されませんでした'
       render :new
@@ -23,11 +27,13 @@ class MessagesController < ApplicationController
   end
 
   def edit
-    @message = Message.find(params[:id])
+    #@message = Message.find(params[:id])
+    #set_message
   end
 
   def update
-    @message = Message.find(params[:id])
+    #@message = Message.find(params[:id])
+    #set_message
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されましたぜ。'
       redirect_to messages_url
@@ -36,17 +42,22 @@ class MessagesController < ApplicationController
       render :edit
     end
   end
+end
 
   def destroy
-    @message = Message.find(params[:id])
+    #@message = Message.find(params[:id])
+    #set_message
     @message.destroy
     
     flash[:success] = 'Message は正常に削除されました。。'
     redirect_to messages_url
   end
-end
 
 private
+#共通メソッドとして。
+def set_message
+  @message = Message.find(params[:id])
+end
 
 # Strong Parameter
 def message_params
